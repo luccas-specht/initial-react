2caee60291093b123f53fde0fbadbe056a89be5b
Luccas Specht <luccasspecht70@gmail.com>
2022-04-20 17:00:28 -0300

    feat: add babel and its config

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// wtf is babel, dude?
+/* 
+   1-> it works to convert available coding on way where all browsers and many different environment could be understand.
+  link: https://babeljs.io
+*/
+
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+};
